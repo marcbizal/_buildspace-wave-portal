6891971dbb3a4d8ae42d952c1f4136304945610d
Marcus Bizal <marc.bizal@gmail.com>
2021-10-13 03:55:16 +0000

    Prepare for typescript

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,5 @@
-require("@nomiclabs/hardhat-waffle");
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -13,9 +14,6 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
+export default {
   solidity: "0.8.4",
 };
