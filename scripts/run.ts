4d359a957eaf682668a4702f3292437236a493bc
Marcus Bizal <marc.bizal@gmail.com>
2021-10-13 04:09:30 +0000

    Update contract to track total waves

--- scripts/run.ts
+++ scripts/run.ts
@@ -1,11 +1,21 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const [owner, randomPerson] = await ethers.getSigners();
   const waveContractFactory = await ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
 
   console.log("Contract deployed to --", waveContract.address);
+  console.log("Contract deployed by --", owner.address);
+
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  waveTxn = await waveContract.connect(randomPerson).wave();
+  await waveTxn.wait();
+
+  await waveContract.getTotalWaves();
 }
 
 main()
